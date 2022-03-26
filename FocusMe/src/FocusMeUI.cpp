3f1f335156f1a027cac47c12ddff75c29beb50ee
JoebobyJoe <83717168+JoebobyJoe@users.noreply.github.com>
2022-03-26 14:46:12 -0500

    Starting towards reading programs
    This is going to be a lot more difficult than I thought it would be, I beleive I have code that will do that (taken from StackOverflow) however I am missing dependencies and such so looking for that

--- FocusMe/src/FocusMeUI.cpp
+++ FocusMe/src/FocusMeUI.cpp
@@ -5,12 +5,72 @@
 #include "Log.h"
 #include <list>
 
+#include <winreg.h>
+#include <iostream>
+#include <winerror.h>
+//#include <WinUser.h>
+
 
 namespace FocusMeUI
 {
     //list of times that the user doesn't want to use program x
     //TODO: this should be passed in when the program launches from a JSON or something
     std::list<TimeSet>times;
+
+    bool EnumInstalledSoftware(void)
+    {
+        HKEY hUninstKey = NULL;
+        HKEY hAppKey = NULL;
+        WCHAR sAppKeyName[1024];
+        WCHAR sSubKey[1024];
+        WCHAR sDisplayName[1024];
+        WCHAR* sRoot = (WCHAR*) L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+        long lResult = ERROR_SUCCESS;
+        DWORD dwType = KEY_ALL_ACCESS;
+        DWORD dwBufferSize = 0;
+
+        //Open the "Uninstall" key.
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, sRoot, 0, KEY_READ, &hUninstKey) != ERROR_SUCCESS)
+        {
+            return false;
+        }
+
+        for (DWORD dwIndex = 0; lResult == ERROR_SUCCESS; dwIndex++)
+        {
+            //Enumerate all sub keys...
+            dwBufferSize = sizeof(sAppKeyName);
+            if ((lResult = RegEnumKeyEx(hUninstKey, dwIndex, sAppKeyName,
+                &dwBufferSize, NULL, NULL, NULL, NULL)) == ERROR_SUCCESS)
+            {
+                //Open the sub key.
+                wsprintf(sSubKey, L"%s\\%s", sRoot, sAppKeyName);
+                if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, sSubKey, 0, KEY_READ, &hAppKey) != ERROR_SUCCESS)
+                {
+                    RegCloseKey(hAppKey);
+                    RegCloseKey(hUninstKey);
+                    return false;
+                }
+
+                //Get the display name value from the application's sub key.
+                dwBufferSize = sizeof(sDisplayName);
+                if (RegQueryValueEx(hAppKey, L"DisplayName", NULL,
+                    &dwType, (unsigned char*)sDisplayName, &dwBufferSize) == ERROR_SUCCESS)
+                {
+                    wprintf(L"%s\n", sDisplayName);
+                }
+                else {
+                    //Display name value doe not exist, this application was probably uninstalled.
+                }
+
+                RegCloseKey(hAppKey);
+            }
+        }
+
+        RegCloseKey(hUninstKey);
+
+        return true;
+    }
+
 
 	void RenderUI()
 	{
